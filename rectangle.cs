f65ded8785e3814570bf5b2238d596beb4b5b216
MartinSahakyan <martinsahakyan3@gmail.com>
2017-12-03 23:05:08 +0400

    adding a project rectangle

---
+++ rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Creating_Rectangle
+{
+    class rectangle
+    {
+            public int height, S, P, width;
+            public int GetArea()
+            {
+                S = height * width;
+                return S;
+            }
+
+            public int GetPerimeter()
+            {
+                P = 2 * (height + width);
+                return P;
+            }
+
+            public void Draw()
+            {
+                for (int i = 0; i < height; i++)
+                {
+                    for (int j = 0; j < width; j++)
+                    {
+                        Console.Write("*");
+                    }
+                    Console.WriteLine();
+                }
+            }
+    }
+}
